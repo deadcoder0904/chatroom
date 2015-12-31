173bbb67aec1a6b821c48c76277b29c04d5bafd4
Akshay Kadam <akshaykadam0904@gmail.com>
2015-12-31 16:09:38 +0530

    Final chatroom with Multiple user & Multiple Chatrooms

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+};
+
+String.prototype.escapeHTML = function() {
+    return this.replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+};
+
 var socket = io();
 var $username = getParameterByName('username');
 var $chatroom = getParameterByName('chatroom');
@@ -6,26 +19,22 @@ var $form = $('#chat-form');
 var $message = $form.find('input[name=message]');
 var $messages = $('#messages');
 
-function getParameterByName(name) {
-    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-}
-
-String.prototype.escapeHTML = function() {
-    return this.replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
-}
+$('#chat-header').html('WELCOME TO ' + $chatroom.escapeHTML());
 
 socket.on('connect',function(){
+    socket.emit('joinRoom',{
+        name: $username,
+        chatroom: $chatroom
+    });
     $messages.append("<div class='page-header text-center text-capitalize text-danger'><b>You have successfully connected to the chatroom " + $chatroom.escapeHTML() + "</b></div>");
 });
 
 socket.on('message',function(message){
     var timestamp = moment.utc(message.timestamp).local();
-    $messages.append('<li class="list-group-item"><b>' + timestamp.format('ddd Do,MMM YYYY @ h:mm a') + '</b><br>' + message.name + ' : ' + message.text + '</li>');
+    if(message.name === 'System')
+        $messages.append('<li class="list-group-item" style="background-color: #6db2d1"><b>' + timestamp.format('ddd Do,MMM YYYY @ h:mm:ss a') + '</b><br>' + message.name + ' : ' + message.text + '</li>');
+    else
+        $messages.append('<li class="list-group-item"><b>' + timestamp.format('ddd Do,MMM YYYY @ h:mm:ss a') + '</b><br>' + message.name + ' : ' + message.text + '</li>');
 });
 
 $form.on('submit',function(event){
@@ -36,7 +45,6 @@ $form.on('submit',function(event){
             name: $username.escapeHTML(),
             text: $message.val().escapeHTML().trim()
         });
-        //$messages.append('<li class="list-group-item"><b>' + $username.escapeHTML() + '</b> : <br> ' + $message.val().escapeHTML().trim() + '</li>');
         $message.val('');
     }
 });
--- server.js
+++ server.js
@@ -4,16 +4,39 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var moment = require('moment');
+var clientInfo = {};
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection',function(socket){
     console.log('User connected to socket.io server');
 
+    socket.on('disconnect',function(){
+        var user = clientInfo[socket.id];
+        if(typeof user !== 'undefined'){
+            socket.leave(user);
+            io.to(user.chatroom).emit('message',{
+                name: 'System',
+                text: user.name + ' has left the Chatroom !!',
+                timestamp: moment().valueOf()
+            });
+        }
+    });
+
+    socket.on('joinRoom',function(req){
+        clientInfo[socket.id] = req;
+        socket.join(req.chatroom);
+        socket.broadcast.to(req.chatroom).emit('message',{
+            name: 'System',
+            text: req.name + ' has joined the Chatroom !!',
+            timestamp: moment().valueOf()
+        });
+    });
+
     socket.on('message',function(message){
         message.timestamp = moment().valueOf();
         console.log(message.timestamp + ' ' + message.name + ' : ' + message.text);
-        io.emit('message',message);
+        io.to(clientInfo[socket.id].chatroom).emit('message',message);
     });
 
     socket.emit('message',{
